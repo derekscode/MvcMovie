e7784722cdf799bfbdea3a73b3c133091218112a
Derek <nospam@gmail.com>
2017-12-15 11:42:11 -0800

    test commit

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -16,6 +16,7 @@ namespace MvcMovie
             Configuration = configuration;
         }
 
+        //test comment
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
